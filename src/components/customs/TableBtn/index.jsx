16f58c86b54d4106afd1c2123402f94eee6516a7
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-06-20 18:19:46 +0430

    color

--- src/components/customs/TableBtn/index.jsx
+++ src/components/customs/TableBtn/index.jsx
@@ -4,11 +4,13 @@ import React from "react";
 
 
 export default function TableBtn({Icon,color="#DA3544",hoverColor="#bb2d3b",...prop}) {
-    const btnClass = `bg-[${color}] hover:bg-[${hoverColor}] py-1 px-2 rounded`
+    // const btnClass = 
 
 
     return (
-        <button className={btnClass} {...prop}><Icon/></button>
+        <>
+            <button className={`bg-[${color}] hover:bg-[${hoverColor}] py-1 px-2 rounded`} {...prop}><Icon/></button>
+        </>
     )
 }
 
