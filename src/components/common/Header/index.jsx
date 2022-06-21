b5a82bc78c7cb1a0e46542c0f12d5f5d3377806b
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-06-21 16:06:34 +0430

    addd

--- src/components/common/Header/index.jsx
+++ src/components/common/Header/index.jsx
@@ -19,14 +19,14 @@ function Header() {
     <header className='h-16 bg-[#6200EA] flex justify-between p-1 text-white'>
         <div className="flex gap-1 items-center">
             <TbListNumbers className="text-2xl"/>
-            <h1 className="font-bold">My To-Do Tasks</h1>
+            <h1 className="font-bold text-[14px]">My To-Do Tasks</h1>
         </div>
         <div className="flex gap-4 items-center px-1">
-            <input placeholder="Search" className="p-2 px-6 text-gray-600 rounded text-[14px] placeholder:text-gray-500 border focus:border-[#86b7fe] outline-0" 
+            <input placeholder="Search" className="p-1 sm:py-2 sm:px-6 text-gray-600 rounded text-[14px] placeholder:text-gray-500 border focus:border-[#86b7fe] outline-0" 
             value={filterValue}
             onChange={filterTodosHandler}/>
-            <FaFilter className="text-[24px]" onClick={()=>dispatch(showFilterMenu())}/>
-            <BsPlusSquareFill className="text-[24px]" onClick={()=>dispatch(showTaskModal())}/>
+            <FaFilter className="text-[18px] sm:text-[24px]" onClick={()=>dispatch(showFilterMenu())}/>
+            <BsPlusSquareFill className="text-[18px] sm:text-[24px]" onClick={()=>dispatch(showTaskModal())}/>
         </div>
     </header>
   )
